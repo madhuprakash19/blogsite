367e8bb79fcb94bbdbd3d4b9a5cd62d93e50c271
madhuprakash19 <madhutp11@gmail.com>
2021-08-10 18:14:06 +0530

    Final working blog app

--- blog/models.py
+++ blog/models.py
@@ -6,7 +6,7 @@ class Post(models.Model):
 	author = models.ForeignKey('auth.User',on_delete=models.CASCADE)
 	title = models.CharField(max_length=200)
 	text = models.TextField()
-	create_date = models.DateTimeField(default=timezone.now())
+	create_date = models.DateTimeField(default=timezone.now)
 	published_date = models.DateTimeField(blank=True,null=True)
 
 	def publish(self):
@@ -14,7 +14,7 @@ class Post(models.Model):
 		self.save()
 
 	def approve_comments(self):
-		return self.comments.filter(approved_comment=True)
+		return self.comment.filter(approved_comment=True)
 
 	def get_absolute_url(self):
 		return reverse("post_detail",kwargs={'pk':self.pk})
--- blog/views.py
+++ blog/views.py
@@ -38,7 +38,7 @@ class PostUpdateView(LoginRequiredMixin,UpdateView):
 
 class PostDeleteView(LoginRequiredMixin,DeleteView):
 	model = Post
-	sucess_url = reverse_lazy('post_list')
+	success_url = reverse_lazy('post_list')
 
 class DraftListView(LoginRequiredMixin,ListView):
 	login_url = '/login/'
@@ -46,14 +46,14 @@ class DraftListView(LoginRequiredMixin,ListView):
 	model = Post
 
 	def get_queryset(self):
-		return Post.objects.filter(published_date__isnull=True).order_by('created_date')
+		return Post.objects.filter(published_date__isnull=True).order_by('create_date')
 
 ##########################################
 ##########################################
 @login_required
 def post_publish(request,pk):
 	post = get_object_or_404(Post,pk=pk)
-	post.publish
+	post.publish()
 	return redirect('post_detail',pk=pk)
 
 @login_required
@@ -65,7 +65,7 @@ def add_comment_to_post(request,pk):
 			comment = form.save(commit=False)
 			comment.post = post
 			comment.save()
-			return redirect('post_deatil',pk=post.pk)
+			return redirect('post_detail',pk=post.pk)
 	else:
 		form = CommentForm()
 	return render(request,'blog/comment_form.html',{'form':form})
@@ -74,7 +74,7 @@ def add_comment_to_post(request,pk):
 def comment_approve(request,pk):
 	comment = get_object_or_404(Comment,pk=pk)
 	comment.approve()
-	return redirect('post_deatil',pk=comment.post.pk)
+	return redirect('post_detail',pk=comment.post.pk)
 
 @login_required
 def comment_remove(request,pk):
